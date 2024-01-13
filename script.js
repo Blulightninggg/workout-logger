54480b52d99a2e77c85cb33e8b66b959ff1500f1
Blulightninggg <155521618+Blulightninggg@users.noreply.github.com>
2024-01-13 05:23:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     updateButtonStyles(savedData);
 
     // Handle click event for the January button
-    document.getElementById("january-image").addEventListener("click", function () {
+    document.getElementById("january-button").addEventListener("click", function () {
         document.getElementById("january-popup-container").style.display = "block";
     });
 
@@ -116,32 +116,3 @@ document.addEventListener("DOMContentLoaded", function () {
         displaySummary();
     }
 });
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
